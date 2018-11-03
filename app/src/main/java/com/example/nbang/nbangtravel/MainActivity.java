a6a3eda03a98476292ee17c7a1ee50c0f2b8d80b
thdud <soyeong1744@gmail.com>
2018-11-03 16:01:23 +0900

    practice merge

--- app/src/main/java/com/example/nbang/nbangtravel/MainActivity.java
+++ app/src/main/java/com/example/nbang/nbangtravel/MainActivity.java
@@ -18,3 +18,4 @@ public class MainActivity extends FragmentActivity {
         getSupportFragmentManager().beginTransaction().add(R.id.menubar_fragment, firstFragment).commit();
     }
 }
+//아무거나 커밋
