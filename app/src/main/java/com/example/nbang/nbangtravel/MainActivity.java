fa0f822e76d822ac1831913c602d8e2dd36b85b2
thdud <soyeong1744@gmail.com>
2018-11-03 15:55:37 +0900

    fragment_trial

--- app/src/main/java/com/example/nbang/nbangtravel/MainActivity.java
+++ app/src/main/java/com/example/nbang/nbangtravel/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.nbang.nbangtravel;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CheckList firstFragment = new CheckList();
+
+        firstFragment.setArguments(getIntent().getExtras());
+        getSupportFragmentManager().beginTransaction().add(R.id.menubar_fragment, firstFragment).commit();
     }
 }
--- app/src/main/java/com/example/nbang/nbangtravel/MenuBar.java
+++ app/src/main/java/com/example/nbang/nbangtravel/MenuBar.java
@@ -1,13 +1,18 @@
 package com.example.nbang.nbangtravel;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MenuBar extends AppCompatActivity{
+
+public class MenuBar extends Fragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main, container, false);
     }
 }
