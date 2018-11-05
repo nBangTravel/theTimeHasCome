f88d92d23fac2313eb7b52d4915ea43cd734561f
thdud <soyeong1744@gmail.com>
2018-11-05 16:09:38 +0900

    fragment 101 - checklist

--- app/src/main/java/com/example/nbang/nbangtravel/MainActivity.java
+++ app/src/main/java/com/example/nbang/nbangtravel/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.nbang.nbangtravel;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CheckList firstFragment = new CheckList();
 
-        firstFragment.setArguments(getIntent().getExtras());
-        getSupportFragmentManager().beginTransaction().add(R.id.menubar_fragment, firstFragment).commit();
+        //Checklist List Update
+       /* ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, "checklist example1");
+        ListView listview = (ListView)findViewById(R.id.list_checklist);
+        listview.setAdapter(adapter);*/
+
+
+        //Fragment assign
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        ChecklistActivity checklistActivity = new ChecklistActivity();
+
+        fragmentTransaction.add(R.id.fragment_checklist, checklistActivity);
     }
 }
-//gh
