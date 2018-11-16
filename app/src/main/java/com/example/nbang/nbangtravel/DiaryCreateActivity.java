e8abf1102ed19d551cae2e096b607c4e98f1b902
dongwon.j <dongwon0207@gmail.com>
2018-11-16 21:37:13 +0900

    add gallery function

--- app/src/main/java/com/example/nbang/nbangtravel/DiaryCreateActivity.java
+++ app/src/main/java/com/example/nbang/nbangtravel/DiaryCreateActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
@@ -11,6 +12,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class DiaryCreateActivity extends AppCompatActivity {
@@ -20,6 +25,7 @@ public class DiaryCreateActivity extends AppCompatActivity {
     private static final int DELETE_ID = Menu.FIRST+1;
     public static int checks = 0;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private final int GALLERY_CODE=1112;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,6 +57,13 @@ public class DiaryCreateActivity extends AppCompatActivity {
 
     }
 
+    public void selectGallery(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_CODE);
+    }
+
     public void onClickcamera(View view){
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -65,5 +78,20 @@ public class DiaryCreateActivity extends AppCompatActivity {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             ((ImageView)findViewById(R.id.diary_create_picture)).setImageBitmap(imageBitmap);
         }
+        else if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+            InputStream in = null;
+            try {
+                in = getContentResolver().openInputStream(data.getData());
+                Bitmap imageBitmap = BitmapFactory.decodeStream(in);
+                in.close();
+                ((ImageView)findViewById(R.id.diary_create_picture)).setImageBitmap(imageBitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void save(){
+
     }
 }
